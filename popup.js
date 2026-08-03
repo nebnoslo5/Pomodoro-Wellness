61a04ab5a425c24d1def110247d4878c7c1c0660
nebnoslo5 <olson.benjamin555@gmail.com>
2026-08-03 16:58:05 -0500

    POMWE_session_1: Start over from scratch and start from a web-page first approach

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('actionBtn').addEventListener('click', () => {
-  const greetingText = document.getElementById('greeting');
-  greetingText.textContent = "Widget button clicked!";
-  greetingText.style.color = "blue";
-});
